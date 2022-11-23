5a65e2bc618dc282b8c58e265d1ff657a6907a06
Kirillos2811 <kirill.sesorov@gmail.com>
2022-11-23 22:16:57 +0300

    Homework 2.2

--- Console Application/Console Application/Console Application.cpp
+++ Console Application/Console Application/Console Application.cpp
@@ -1,7 +1,21 @@
-﻿#include <iostream>
+﻿#include <stdio.h>
 
-int main()
-{
-    printf("Hello");
+int main(){	
+	int a, b;
+	printf("a = ");
+	scanf_s("%i", &a);
+	printf("b = ");
+	scanf_s("%i", &b);
+	if (a == b) {
+		printf("Equal");
+	}
+	else {
+		if (a > b) {
+			printf("Bigger");
+		}
+		else {
+			printf("Smaller");
+		}
+	}
 }
 
