f3e45bda2c3a17badc59ef42fcf27ad715814e5f
Kirillos2811 <kirill.sesorov@gmail.com>
2022-12-02 00:20:08 +0300

    homework 2.6

--- Console Application/Console Application/Console Application.cpp
+++ Console Application/Console Application/Console Application.cpp
@@ -1,49 +1,124 @@
 ﻿#include <stdio.h>
 
 int main(){	
-	int day;
-	printf("Day number = ");
+	int day, month, year;
+	printf("Your date of birth:\n");
+	printf("Day? ");
 	scanf_s("%i", &day);
-	switch(day)
+	printf("Month? ");
+	scanf_s("%i", &month);
+	printf("Year? ");
+	scanf_s("%i", &year);
+
+	if (year % 4 == 0) {
+		printf("Високосный год\n");
+	}
+	else {
+		printf("Невисокосный год\n");
+	}
+
+	printf("Год по китайскому календарю:\n");
+	switch(year%12)
 	{
+	case 0:
+	{
+		printf("Обезьяны");
+	}
+	break;
 	case 1:
 	{
-		printf("Monday");
+		printf("Петуха");
 	}
 	break;
 	case 2:
 	{
-		printf("Tuesday");
+		printf("Собаки");
 	}
 	break;
 	case 3:
 	{
-		printf("Wednesday");
+		printf("Свиньи");
 	}
 	break;
 	case 4:
 	{
-		printf("Thursday");
+		printf("Крысы");
 	}
 	break;
 	case 5:
 	{
-		printf("Friday");
+		printf("Быка");
 	}
 	break;
 	case 6:
 	{
-		printf("Saturday");
+		printf("Тигра");
 	}
 	break;
 	case 7:
 	{
-		printf("Sunday");
+		printf("Кролика");
 	}
 	break;
-	default:{
-		printf("Error");
+	case 8:
+	{
+		printf("Дракона");
 	}
+	break;
+	case 9:
+	{
+		printf("Змеи");
+	}
+	break;
+	case 10:
+	{
+		printf("Лошади");
+	}
+	break;
+	case 11:
+	{
+		printf("Овцы");
+	}
+	}
+	printf("\nЗнак зодиака\n");
+	if ((month == 12 and day > 19) || (month == 1 and day < 22)) {
+		printf("Козерог");
+	}
+	else if ((month == 1 and day > 19) || (month == 2 and day < 22)) {
+		printf("Водолей");
+	}
+	else if ((month == 2 and day > 19) || (month == 3 and day < 22)) {
+		printf("Рыбы");
+	}
+	else if ((month == 3 and day > 19) || (month == 4 and day < 22)) {
+		printf("Овен");
+	}
+	else if ((month == 4 and day > 19) || (month == 5 and day < 22)) {
+		printf("Телец");
+	}
+	else if ((month == 5 and day > 19) || (month == 6 and day < 22)) {
+		printf("Близнецы");
+	}
+	else if ((month == 6 and day > 19) || (month == 7 and day < 22)) {
+		printf("Рак");
+	}
+	else if ((month == 7 and day > 19) || (month == 8 and day < 22)) {
+		printf("Лев");
+	}
+	else if ((month == 8 and day > 19) || (month == 9 and day < 22)) {
+		printf("Девы");
+	}
+	else if ((month == 9 and day > 19) || (month == 10 and day < 22)) {
+		printf("Весы");
+	}
+	else if ((month == 10 and day > 19) || (month == 11 and day < 22)) {
+		printf("Скорпион");
+	}
+	else if ((month == 11 and day > 19) || (month == 12 and day < 22)) {
+		printf("Стрелец");
+	}
+	else {
+		printf("Error");
 	}
 }
 
