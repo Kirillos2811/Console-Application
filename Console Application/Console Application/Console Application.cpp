929346a7876b91b755b5a2cacff6a00ab4134e7e
Kirillos2811 <kirill.sesorov@gmail.com>
2022-12-05 22:45:48 +0300

    homework 3.6 task 1

--- Console Application/Console Application/Console Application.cpp
+++ Console Application/Console Application/Console Application.cpp
@@ -1,26 +1,8 @@
 ﻿#include <stdio.h>
 
 int main(){	
-	//program number 1
-	printf("program number 1\n");
-	int i = 0;
-	while (i < 10) {
-		printf("Hello\n");
-		i = i + 1;
-	}
-	//program number 2
-	printf("program number 2\n");
-	i = 0;
-	while (i < 15) {
-		printf("%i ", i);
-		i = i + 1;
-	}
-	//program number 3
-	printf("\nprogram number 3\n");
-	i = 2;
-	while (i < 15) {
-		printf("%i ", i);
-		i = i + 2;
+	for (int i = 1; i < 10; i++) {
+		printf("5 * %i = %i\n", i, 5 * i);
 	}
 }
 
