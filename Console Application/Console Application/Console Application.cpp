7b2bfaf1801ad09a53297e045ce88a25405c312c
Kirillos2811 <kirill.sesorov@gmail.com>
2022-12-09 22:53:04 +0300

    homework 3.8

--- Console Application/Console Application/Console Application.cpp
+++ Console Application/Console Application/Console Application.cpp
@@ -1,10 +1,29 @@
 ﻿#include <stdio.h>
 
 int main(){	
-	int sum = 0;
-	for (int i = 1; i < 101; i++) {
-		sum += i;
+	int min, max, n;
+	float num;
+	float sum = 0;
+	printf("Numbers amount? ");
+	scanf_s("%f", &num);
+	scanf_s("%i", &n);
+	sum += n;
+	min = n;
+	max = n;
+
+	for (int i = 0; i < num-1; i++) {
+		scanf_s("%i", &n);
+		if (n < min) {
+			min = n;
+		}
+		if (n > max) {
+			max = n;
+		}
+		sum += n;
 	}
-	printf("Sum:%i", sum);
+	printf("Max: %i\n", max);
+	printf("Min: %i\n", min);
+	printf("Sum: %f\n", sum);
+	printf("Average: %f", sum / num);
 }
 
