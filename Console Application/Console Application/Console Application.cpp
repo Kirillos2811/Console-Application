29be49294cc5aaf703e0e9cae052ffbb73f788cb
Kirillos2811 <kirill.sesorov@gmail.com>
2022-12-13 21:17:27 +0300

    homework 4.3

--- Console Application/Console Application/Console Application.cpp
+++ Console Application/Console Application/Console Application.cpp
@@ -1,14 +1,29 @@
 ﻿#include <stdio.h>
 
 int main(){	
-	int arr[7];
-	printf("Enter the numbers:\n");
-	for(int i = 0; i < 7; i++) {
+	int arr[100];
+	int n;
+	printf("Enter the amount of elements: ");
+	scanf_s("%i", &n);
+	printf("Enter the array:\n");
+	for(int i = 0; i < n; i++) {
 		scanf_s("%i", &arr[i]);
 	}
-	printf("Entered numbers:\n");
-	for (int i = 0; i < 7; i++) {
-		printf("%i\n", arr[i]);
+	bool status = true;
+	while (status) {
+		status = false;
+		for (int i = 0; i < n - 1; i++) {
+			if (arr[i] > arr[i + 1]) {
+				status = true;
+				int c = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = c;
+			}
+		}
+	}
+	printf("Sorted array:\n");
+	for (int i = 0; i < n; i++) {
+		printf("%4i", arr[i]);
 	}
 }
 
