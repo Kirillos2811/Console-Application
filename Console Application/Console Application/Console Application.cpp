4f3c26829fa995b9eabb055b2d6cd5216c222b0b
Kirillos2811 <kirill.sesorov@gmail.com>
2022-12-14 19:33:05 +0300

    homework 4.4

--- Console Application/Console Application/Console Application.cpp
+++ Console Application/Console Application/Console Application.cpp
@@ -2,28 +2,26 @@
 
 int main(){	
 	int arr[100];
-	int n;
+	int n, max, min, sum;
+	float average;
 	printf("Enter the amount of elements: ");
 	scanf_s("%i", &n);
 	printf("Enter the array:\n");
 	for(int i = 0; i < n; i++) {
 		scanf_s("%i", &arr[i]);
 	}
-	bool status = true;
-	while (status) {
-		status = false;
-		for (int i = 0; i < n - 1; i++) {
-			if (arr[i] > arr[i + 1]) {
-				status = true;
-				int c = arr[i];
-				arr[i] = arr[i + 1];
-				arr[i + 1] = c;
-			}
-		}
-	}
-	printf("Sorted array:\n");
+	max = min = arr[0];
+	sum = 0;
 	for (int i = 0; i < n; i++) {
-		printf("%4i", arr[i]);
+		if (arr[i] > max) {
+			max = arr[i];
+		}
+		if (arr[i] < min) {
+			min = arr[i];
+		}
+		sum += arr[i];
 	}
+	average = (float)sum / (float)n;
+	printf("Min = %i; max = %i; sum = %i; average = %f", min, max, sum, average);
 }
 
