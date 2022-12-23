d859aa9d98444d7b5bc281c3e63cca1ffbf16ce0
Kirillos2811 <kirill.sesorov@gmail.com>
2022-12-24 02:51:33 +0300

    homework 5.5

--- Console Application/Console Application/Console Application.cpp
+++ Console Application/Console Application/Console Application.cpp
@@ -5,17 +5,40 @@
 int main(){	
 	int arr[100][100];
 	int n, m;
+
 	printf("Enter the amount of rows: ");
 	scanf_s("%i", &n);
+
 	printf("Enter the amount of columns: ");
 	scanf_s("%i", &m);
-	srand((unsigned int) time(NULL));
+
+	printf("Enter the elements:\n");
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			arr[i][j] = rand()%(7-(-2)+1)+(-2);
+			scanf_s("%i", &arr[i][j]);
 		}
 	}
-	printf("Random elements of the array:\n");
+
+	printf("Entered array:\n");
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			printf("%4i", arr[i][j]);
+		}
+		printf("\n");
+	}
+	int temp_m = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < temp_m; j++) {
+			if (i != j) {
+				int buff = arr[i][j];
+				arr[i][j] = arr[j][i];
+				arr[j][i] = buff;
+			}
+		}
+		temp_m++;
+	}
+
+	printf("Inversed array:\n");
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			printf("%4i", arr[i][j]);
