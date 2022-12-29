94d7d8b281f4632e95ebee9d6b43734060da8cfb
Kirillos2811 <kirill.sesorov@gmail.com>
2022-12-30 02:22:01 +0300

    homework 6.3 task 1

--- Console Application/Console Application/Console Application.cpp
+++ Console Application/Console Application/Console Application.cpp
@@ -1,52 +1,10 @@
 ﻿#include <stdio.h>
+#include <string>
 
-int main(){	
-	int arr[100][100];
-	int n, m;
-
-	printf("Enter the amount of rows: ");
-	scanf_s("%i", &n);
-
-	printf("Enter the amount of columns: ");
-	scanf_s("%i", &m);
-
-	printf("Enter the elements:\n");
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			scanf_s("%i", &arr[i][j]);
-		}
-	}
-
-	printf("Entered array:\n");
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			printf("%4i", arr[i][j]);
-		}
-		printf("\n");
-	}
-
-	bool flag;
-	for (int i = 0; i < n; i++) {
-		flag = true;
-		while(flag) {
-			flag = false;
-			for (int j = 0; j < m - 1; j++) {
-				if (arr[i][j] > arr[i][j + 1]) {
-					flag = true;
-					int buff = arr[i][j];
-					arr[i][j] = arr[i][j + 1];
-					arr[i][j + 1] = buff;
-				}
-			}
-		}
-	}
-
-	printf("Sorted array:\n");
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			printf("%4i", arr[i][j]);
-		}
-		printf("\n");
-	}
+int main()
+{
+    char str[100];
+    gets_s(str);
+    printf("Second element of the string:%c", str[1]);
 }
 
