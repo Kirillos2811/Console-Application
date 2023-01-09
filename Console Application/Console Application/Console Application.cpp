024170cff33d9c5b08e426930caee4be605d1830
Kirillos2811 <kirill.sesorov@gmail.com>
2023-01-09 19:01:52 +0300

    Homework 6.4 task 1

--- Console Application/Console Application/Console Application.cpp
+++ Console Application/Console Application/Console Application.cpp
@@ -4,7 +4,17 @@
 int main()
 {
     char str[100];
+    printf("Enter a string:");
     gets_s(str);
-    printf("Second element of the string:%c", str[1]);
+    printf("Input string:");
+    puts(str);
+
+    int str_len = strlen(str);
+    for (int i = 0; i < str_len; i++) {
+        str[i] = str[i + 1];
+    }
+
+    printf("Output string:");
+    puts(str);
 }
 
