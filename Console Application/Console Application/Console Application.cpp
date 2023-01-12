10c448a0e96e22a7b360fd7fa64a78b76d6c2b87
Kirillos2811 <kirill.sesorov@gmail.com>
2023-01-12 22:27:52 +0300

    Homework 7.3

--- Console Application/Console Application/Console Application.cpp
+++ Console Application/Console Application/Console Application.cpp
@@ -1,29 +1,98 @@
 ﻿#include <stdio.h>
 #include <string>
-#include <time.h>
+#include <math.h>
 
-void assign_values_and_print(int arr[], int len) {
-    for (int i = 0; i < len; i++) {
-        arr[i] = rand();
+float square_root(int a) {
+    float l = 0;
+    float r = 1e10;
+    while (r - l > 1e-5) {
+        float m = l + (r - l) / 2;
+        if (m * m > a) {
+            r = m;
+        } 
+        else if (m * m < a) {
+            l = m;
+        }
+        else if (m * m == a) {
+            return m;
+        }
     }
+    return l;
+}
 
-    for (int i = 0; i < len; i++) {
-        printf("%i\n", arr[i]);
+void square_root(int a, float* result) {
+    float l = 0;
+    float r = 1e10;
+    while (r - l > 1e-5) {
+        float m = l + (r - l) / 2;
+        if (m * m > a) {
+            r = m;
+        }
+        else if (m * m < a) {
+            l = m;
+        }
+        else if (m * m == a) {
+            *result = m;
+        }
     }
+    *result = l;
+}
+
+void square_root(int a, float &result) {
+    float l = 0;
+    float r = 1e10;
+    while (r - l > 1e-5) {
+        float m = l + (r - l) / 2;
+        if (m * m > a) {
+            r = m;
+        }
+        else if (m * m < a) {
+            l = m;
+        }
+        else if (m * m == a) {
+            result = m;
+        }
+    }
+    result = l;
 }
 
 int main()
 {   
-    int a[5];
-    int b[10];
+    int a, b, c;
+    printf("Enter the first number: ");
+    scanf_s("%i", &a);
 
-    srand(time(NULL));
-    
-    printf("Elements of array number 1:\n");
-    assign_values_and_print(a, 5);
+    printf("Enter the second number: ");
+    scanf_s("%i", &b);
 
-    printf("Elements of array number 2:\n");
-    assign_values_and_print(b, 10);
+    printf("Enter the third number: ");
+    scanf_s("%i", &c);
+
+    float a_square_root = square_root(a);
+    if (abs(a_square_root - round(a_square_root)) < 1e-4) {
+        printf("Fisrt number has a natural square root\n");
+    }
+    else {
+        printf("Fisrt number doesn't have a natural square root\n");
+    }
+
+    float b_square_root;
+    square_root(b, &b_square_root);
+    if (abs(b_square_root - round(b_square_root)) < 1e-4) {
+        printf("Second number has a natural square root\n");
+    }
+    else {
+        printf("Second number doesn't have a natural square root\n");
+    }
+   
+    float c_square_root;
+    square_root(c, c_square_root);
+    if (abs(c_square_root - round(c_square_root)) < 1e-4) {
+        printf("Third number has a natural square root\n");
+    }
+    else {
+        printf("Third number doesn't have a natural square root\n");
+    }
 
     return 0;
 }
