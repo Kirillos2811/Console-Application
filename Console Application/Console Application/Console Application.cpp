2b3e4aeea75ef05b5bc8ddb6c979fe156c33b08d
Kirillos2811 <kirill.sesorov@gmail.com>
2023-01-13 19:08:01 +0300

    Homework 7.4 task 1

--- Console Application/Console Application/Console Application.cpp
+++ Console Application/Console Application/Console Application.cpp
@@ -1,99 +1,25 @@
 ﻿#include <stdio.h>
 #include <string>
-#include <math.h>
 
-float square_root(int a) {
-    float l = 0;
-    float r = 1e10;
-    while (r - l > 1e-5) {
-        float m = l + (r - l) / 2;
-        if (m * m > a) {
-            r = m;
-        } 
-        else if (m * m < a) {
-            l = m;
-        }
-        else if (m * m == a) {
-            return m;
-        }
-    }
-    return l;
-}
+void change_chars_in_string() {
+    char str[100];
+    char symb;
 
-void square_root(int a, float* result) {
-    float l = 0;
-    float r = 1e10;
-    while (r - l > 1e-5) {
-        float m = l + (r - l) / 2;
-        if (m * m > a) {
-            r = m;
-        }
-        else if (m * m < a) {
-            l = m;
-        }
-        else if (m * m == a) {
-            *result = m;
-        }
-    }
-    *result = l;
-}
+    printf("Enter a string:");
+    gets_s(str);
+    printf("Enter a symbol:");
+    scanf_s("%c", &symb, 1);
 
-void square_root(int a, float &result) {
-    float l = 0;
-    float r = 1e10;
-    while (r - l > 1e-5) {
-        float m = l + (r - l) / 2;
-        if (m * m > a) {
-            r = m;
-        }
-        else if (m * m < a) {
-            l = m;
-        }
-        else if (m * m == a) {
-            result = m;
-        }
-    }
-    result = l;
+    int len = strlen(str);
+    str[0] = symb;
+    str[len - 1] = symb;
+    
+    printf("Output string:");
+    puts(str);
 }
 
 int main()
-{   
-    int a, b, c;
-    printf("Enter the first number: ");
-    scanf_s("%i", &a);
-
-    printf("Enter the second number: ");
-    scanf_s("%i", &b);
-
-    printf("Enter the third number: ");
-    scanf_s("%i", &c);
-
-    float a_square_root = square_root(a);
-    if (abs(a_square_root - round(a_square_root)) < 1e-4) {
-        printf("Fisrt number has a natural square root\n");
-    }
-    else {
-        printf("Fisrt number doesn't have a natural square root\n");
-    }
-
-    float b_square_root;
-    square_root(b, &b_square_root);
-    if (abs(b_square_root - round(b_square_root)) < 1e-4) {
-        printf("Second number has a natural square root\n");
-    }
-    else {
-        printf("Second number doesn't have a natural square root\n");
-    }
-   
-    float c_square_root;
-    square_root(c, c_square_root);
-    if (abs(c_square_root - round(c_square_root)) < 1e-4) {
-        printf("Third number has a natural square root\n");
-    }
-    else {
-        printf("Third number doesn't have a natural square root\n");
-    }
-
-    return 0;
+{  
+    change_chars_in_string();
 }
 
