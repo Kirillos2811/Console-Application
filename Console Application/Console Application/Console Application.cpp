ec892b5772f0901ff19076e528da0378447b1ce0
Kirillos2811 <kirill.sesorov@gmail.com>
2023-01-17 15:53:09 +0300

    Homework 8.1 task 1

--- Console Application/Console Application/Console Application.cpp
+++ Console Application/Console Application/Console Application.cpp
@@ -1,25 +1,100 @@
 ﻿#include <stdio.h>
-#include <string>
 
-void change_chars_in_string() {
-    char str[100];
-    char symb;
+class Person
+{	
+protected:
+	const char* name;
+	int age;
+	bool male;
+	float weight;
 
-    printf("Enter a string:");
-    gets_s(str);
-    printf("Enter a symbol:");
-    scanf_s("%c", &symb, 1);
+public: 
+	Person(char name[], int age, bool male, float weight) 
+	{
+		this->name = name;
+		this->age = age;
+		this->male = male;
+		this->weight = weight;
+	}
 
-    int len = strlen(str);
-    str[0] = symb;
-    str[len - 1] = symb;
-    
-    printf("Output string:");
-    puts(str);
-}
+	const char* getName() 
+	{
+		return name;
+	}
+	void setName(char name[]) 
+	{
+		this->name = name;
+	}
+
+	int getAge() 
+	{
+		return age;
+	}
+	void setAge(int age) 
+	{
+		this->age = age;
+	}
+
+	const char * isMale() 
+	{
+		if (male == true) 
+		{
+			return "true";
+		}
+		else {
+			return "false";
+		}
+	}
+	void setGender(bool male) 
+	{
+		this->male = male;
+	}
+
+	float getWeight() 
+	{
+		return weight;
+	}
+	void setWeight(float weight) 
+	{
+		this->weight = weight;
+	}
+};
+
+class Student : public Person
+{
+protected:
+	int educational_course;
+
+public:
+	Student(char name[], int course, int age, bool male, float weight) : Person(name, age, male, weight)
+	{
+		educational_course = course;
+	}
+
+	int getCourse() 
+	{
+		return educational_course;
+	}
+	void setCourse(int course) 
+	{
+		educational_course = course;
+	}
+};
 
 int main()
 {  
-    change_chars_in_string();
+	char name[] = "Jack";
+	Student student = Student(name, 1, 22, true, 85);
+	printf("Student name: %s; course: %i; age: %i; Male gender: %s; weight: %f\n",
+		student.getName(), student.getCourse(), student.getAge(), student.isMale(), student.getWeight());
+
+	printf("A year later\n");
+
+	student.setCourse(2);
+	student.setAge(23);
+	student.setWeight(98);
+	printf("Student name: %s; course: %i; age: %i; Male gender: %s; weight: %f",
+		student.getName(), student.getCourse(), student.getAge(), student.isMale(), student.getWeight());
+	return 0;
 }
 
